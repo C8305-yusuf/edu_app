50dfd2e1803b0b656b57c52c2f9b95d5cc8aaf13
c8305_yusuf <kiowa297@gmail.com>
2021-11-30 22:58:24 +0300

    feat : added messages

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .forms import ContactForm
+from django.contrib import messages
+
 
 # Create your views here.
 
@@ -10,11 +12,20 @@ def home(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, "form has been send")
+            return redirect("home")
+    else:
+        form = ContactForm(request.POST)
+        
     context = {
         'form': form
         
     }        
     return render(request, "home/index.html", context)
+
+
+
+
 
 def about(request):
     return render(request, "home/about.html")
